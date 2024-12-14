8a72f8b50f2e4dc0a0d478e4125991564386c961
AbeerSwadi <alaalsalam101@gmail.com>
2024-12-14 11:55:03 +0100

    add travel table detiles

---
+++ travels/travels/doctype/trip_routes/test_trip_routes.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, SALA and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTripRoutes(FrappeTestCase):
+	pass
---
+++ travels/travels/doctype/truck_driver/truck_driver.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, SALA and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Truck Driver", {
+// 	refresh(frm) {
+
+// 	},
+// });
